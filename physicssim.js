afcd823f5195943ed0d07a0aeef11700dc773107
ACromack <ashleydavidcromack@gmail.com>
2020-09-12 19:58:15 +0100

    Object Orientation
    Make the object oriented, allowing us to create multiple boxes (which is done on button click)
    
    Screen is currently looping, next step is to make boxes constrained to the canvas.

--- physicssim.js
+++ physicssim.js
@@ -1,68 +1,102 @@
-// var ctx = document.getElementById("canvas").getContext("2d");
-
-// //Create Var
-// var circ = new Circle(100, 100, 20);
-
-// //Define the circle
-// function Circle(x, y, r) {
-//     "use strict";
-//     this.x = (x === null) ? 0 : x;
-//     this.y = (y === null) ? 0 : y;
-//     this.r = (r === null) ? 0 : r;
-    
-//     this.fill = function(ctx) {
-//         ctx.beginPath();
-//         ctx.arc(this.x, this.y, this.r, 0, Math.PI*2);
-//         ctx.fill();
-//     }
-// }
-
-// //Draw the circle as object
-// circ.fill(ctx);
-
-// ------------------
-
 var canvas = document.getElementById("canvas");
 var width = canvas.width;
 var height = canvas.height;
 var ctx = canvas.getContext("2d");
-ctx.fillStyle = "red";
 
-var state = {
-    x: width/2,
-    y: height/2
-}
+// Initialise an array to hold the physical objects
+var physicalObjects = [];
 
-function move(dt){
-     
-}
-
-function update(progress){
-    // Update the state of the game
-    state.x += progress;
-
-    if(state.x > width){
-        state.x -= width;
+var PhysicalObject = function(x, y, w, h) 
+{
+    // Set the object's x/y position
+    this.x = x;
+    this.y = y;
+    
+    // Set the object's width and height
+    this.width = w;
+    this.height = h;
+    
+    // Initialise the object's x and y velocity with a value of 0 (it's stationary initially)
+    this.xVel = 0.1;
+    this.yVel = 0.1;
+    
+    // Adjust the object's x velocity
+    this.addXVel = function(vel) { 
+        this.xVel += vel;
+    };
+    
+    // Adjust the object's y velocity
+    this.addYVel = function(vel) { 
+        this.yVel += vel;
+    };
+    
+    // Update the object's position for the next frame
+    this.nextFrame = function() { 
+        this.x += this.xVel;
+        this.y += this.yVel;
     }
+} 
 
+function screenLoop(obj) 
+{    
+    // Drifted off of right edge 
+    if (obj.x - (obj.width / 2) > canvas.width)
+        obj.x = -obj.width / 2;
+    
+    // Drifted off of left edge
+    if (obj.x + (obj.width / 2) < 0)
+        obj.x = canvas.width + obj.width / 2;
+    
+    // Drifted off of bottom edge 
+    if (obj.y - (obj.height / 2) > canvas.height)
+        obj.y = -obj.height / 2;
+    
+    // Drifted off of top edge
+    if (obj.y + (obj.height / 2) < 0)
+        obj.y = canvas.height + obj.height / 2;
 }
 
-function draw(){
-    // Draw the state of the game
-    ctx.clearRect(0, 0, width, height)
-
-    ctx.fillRect(state.x - 5, state.y - 5, 10, 10)
+// Function to create a new box in the canvas (on clicking button in html)
+function createBox(){
+    // Start the render loop
+    frameRenderLoop(); 
+        
+    // Add an object into the engine at x: 100, y: 100, with a width and height of 20 pixels.
+    physicalObjects.push(new PhysicalObject(100, 100, 20, 20));  
+        
+    // Give it a little push!
+    physicalObjects[0].addXVel(0.1);
 }
 
-function loop(timestamp){
-    var progress = timestamp - lastRender;
 
-    update(progress);
-    draw();
+frameRender = function() 
+{
+    // Clear view
+    ctx.clearRect(0, 0, width, height);
+    
+    // For each object in the physicalObjects array...
+    for (var i = 0; i < physicalObjects.length; i++) {
+        
+        // Draw a rectangle on the canvas to represent the object, based on the object's x, y, width and height
+        ctx.fillRect(
+            physicalObjects[i].x, 
+            physicalObjects[i].y, 
+            physicalObjects[i].width, 
+            physicalObjects[i].height
+        );
 
-    lastRender = timestamp;
-    window.requestAnimationFrame(loop);
+        screenLoop(physicalObjects[i]);
+            
+        // Tell the object to update itself for the next frame
+        physicalObjects[i].nextFrame();
+    }
+} 
+     
+frameRenderLoop = function() 
+{
+    // Use requestAnimationFrame to trigger the 'frameRenderLoop' function as soon as the browser is ready to repaint
+    requestAnimationFrame(frameRenderLoop);
+        
+    // Render the current frame
+    frameRender();
 }
-
-var lastRender = 0;
-window.requestAnimationFrame(loop);
